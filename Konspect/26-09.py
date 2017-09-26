23152438e4a0a2f19447e230c827ba584e0768e7
Анна Полянская <31651460+polyankaglade@users.noreply.github.com>
2017-09-26 11:44:39 +0300

    Update 26-09.py

--- Konspect/26-09.py
+++ Konspect/26-09.py
@@ -1,2 +1,20 @@
+# 1
 for i in range(1, 30, 2):
     print (i)
+
+# 2 
+i = 9
+while i >= 5:
+    print(i)
+    i -= 1
+   
+# 3
+# шифр Цезаря
+shifr = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяаб   '
+a = input('enter a word')
+ans = ''
+for x in a :
+    b = shifr.find(x)
+    c = shifr[b+2]
+    ans += c
+print(ans)
