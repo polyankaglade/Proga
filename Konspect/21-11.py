cb79deabdfc118b21cc05c1134b98c4208d1c5ec
Анна Полянская <31651460+polyankaglade@users.noreply.github.com>
2017-11-21 11:41:39 +0300

    Update 21-11.py

--- Konspect/21-11.py
+++ Konspect/21-11.py
@@ -4,3 +4,19 @@ half_ind = (len(line)+1)//2
 print(half_ind)
 new_line = line[half_ind:] + line[:half_ind]
 print(new_line)
+
+
+while True:
+    line = input('enter smth: ')
+    symb = 'a'
+    first = line.find(symb)
+    last = line.rfind(symb)
+    if line == 'finish':
+        break
+
+    if first == -1:
+        print('nothing found')
+    elif first == last:
+        print('one time, number', first)
+    else:
+        print('first:',first, 'last:', last)
