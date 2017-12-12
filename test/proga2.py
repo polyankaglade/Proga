f622bc906458f8c1434c90bb704494a5b69b844a
Анна Полянская <31651460+polyankaglade@users.noreply.github.com>
2017-12-12 13:30:54 +0300

    Update proga2.py

--- test/proga2.py
+++ test/proga2.py
@@ -2,7 +2,6 @@ endangered = 0
 with open("Extinct_languages.tsv", "r", encoding="utf-8") as f:
     for line in f:
         stroka = line.split("\t")
-        #print(stroka)
         if stroka[2] == 'Critically endangered\n':
             endangered += 1
         else:
