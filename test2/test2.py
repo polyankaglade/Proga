b984956059d235f98db69369918a8471215b1176
Анна Полянская <31651460+polyankaglade@users.noreply.github.com>
2018-04-07 12:02:50 +0300

    Add files via upload

---
+++ test2/test2.py
@@ -0,0 +1,40 @@
+import re
+
+def open_file():
+    with open ("mystem.xml", encoding="utf-8") as f:
+        text = f.read()
+    return text
+
+def search(text):
+    result = re.findall("<w><ana lex=\"(?:.)*?\" gr=\"(.*?)\"",text)
+    return result
+
+def create_dic(gram):
+    dic = {}
+    for i in gram:
+        n = 0
+        for x in gram:
+            if i == x:
+                n += 1
+            else:
+                continue
+        s = {i:n}
+        dic.update(s)
+    return dic
+            
+def write_results(dic):
+    
+    with open("result.txt","w", encoding="utf-8") as f:
+        for i,x in dic.items():
+            i = str(i)
+            x = str(x)
+            s = ''
+            s = i + " : "+ x + "\n"
+            f.write(s)
+    return f
+
+def main():
+    write_results(create_dic(search(open_file())))
+    print("done")
+
+main()
